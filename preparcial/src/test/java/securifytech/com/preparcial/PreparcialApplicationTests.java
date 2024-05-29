bf7cf83d5b948300c7dc6fef07cbfed276bfdff5
davidperlera2 <00165220@uca.edu.sv>
2024-05-28 21:59:15 -0600

    first commit

---
+++ preparcial/src/test/java/securifytech/com/preparcial/PreparcialApplicationTests.java
@@ -0,0 +1,13 @@
+package securifytech.com.preparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreparcialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
